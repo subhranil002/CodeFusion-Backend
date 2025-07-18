16cd9a4e046b1bd3b124b2c2fe5e52d8adfdb79a
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-07-18 18:52:48 +0530

    Convert connection to websocket only

--- src/configs/socket.config.ts
+++ src/configs/socket.config.ts
@@ -3,5 +3,8 @@ import http from "http";
 import constants from "../constants.js";
 
 export default function initSocket(server: http.Server) {
-    return new Server(server, { cors: { origin: constants.FRONTEND_URL } });
+    return new Server(server, { 
+        cors: { origin: "*" },
+        transports: ["websocket"],
+    });
 }
